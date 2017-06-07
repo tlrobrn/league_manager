666ffc928fab6b503620fe5970f5e8459a74ad03
tlrobrn <obrien.taylorm@gmail.com>
2017-06-06 22:26:27 -0500

    interleave home and away games

--- web/services/schedule_service.ex
+++ web/services/schedule_service.ex
@@ -9,16 +9,17 @@ defmodule LeagueManager.ScheduleService do
 
     Repo.transaction fn ->
       schedule_games(pinned_team, teams, 1, rounds)
-      schedule_games(pinned_team, teams, 1, rounds, rounds, true)
+      schedule_games(pinned_team, teams, 1, rounds, rounds)
     end
   end
 
   defp pad_teams(teams) when rem(length(teams), 2) == 0, do: [nil | teams]
   defp pad_teams(teams), do: teams
 
-  defp schedule_games(pinned_team, teams, round, max_round, offset \\ 0, away_first \\ false)
-  defp schedule_games(_, _, round, max_round,_ , _) when round > max_round, do: nil
-  defp schedule_games(pinned_team, teams, round, max_round, offset, away_first) do
+  defp schedule_games(pinned_team, teams, round, max_round, offset \\ 0)
+  defp schedule_games(_, _, round, max_round,_ ) when round > max_round, do: nil
+  defp schedule_games(pinned_team, teams, round, max_round, offset) do
+    away_first = (rem(round + offset, 2) == 1)
     [tribute | pool] = teams
     create_game({pinned_team, tribute}, round + offset, away_first)
 
@@ -26,7 +27,7 @@ defmodule LeagueManager.ScheduleService do
     bottom = Enum.reverse(bottom)
     Stream.zip(top, bottom) |> Enum.map(&create_game(&1, round + offset, away_first))
 
-    schedule_games(pinned_team, pool ++ [tribute], round + 1, max_round, offset, away_first)
+    schedule_games(pinned_team, pool ++ [tribute], round + 1, max_round, offset)
   end
 
   defp create_game({nil, _}, _, _), do: nil
