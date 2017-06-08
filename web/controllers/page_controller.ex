490a12a0da87021c38910afc300d4d3d7f7d6bde
tlrobrn <obrien.taylorm@gmail.com>
2017-06-08 13:12:17 -0500

    set silver and bronze medals once teams are locked in to 2nd and 3rd place

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,16 +4,7 @@ defmodule LeagueManager.PageController do
   def index(conn, _params) do
     games = LeagueManager.Game.current_games()
     records = LeagueManager.Team.records_with_teams_and_potential_points() |> Enum.to_list
-    [{{_, _}, %{points: leader_points}} | [{{_, nearest_potential_points}, %{points: second_points}} | _]] = records
-
-    render(
-      conn,
-      "index.html",
-      games: games,
-      records: records,
-      leader_points: leader_points,
-      clinched_league: leader_points > nearest_potential_points + second_points
-    )
+    render(conn, "index.html", games: games, records: records)
   end
 
   def schedule(conn, _params) do
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,38 @@
 defmodule LeagueManager.PageView do
   use LeagueManager.Web, :view
+  alias LeagueManager.Team.Record
+
+  def league_table(data) do
+    [_first, _second, third | _others] = data
+    third_place_minimum = get_current_points(third)
+
+    clinched = data |> determine_if_clinched
+    eliminated = data
+    |> Stream.map(fn record ->
+      max_points = get_potential_total_points(record)
+      max_points < third_place_minimum
+    end)
+
+    Stream.zip(clinched, eliminated)
+    |> Stream.zip(data)
+  end
+
+  defp get_current_points({{_team, _potential_points}, %Record{points: points}}), do: points
+
+  defp get_potential_total_points({{_team, potential_points}, %Record{points: points}}), do: potential_points + points
+
+  defp determine_if_clinched(data, prev_points \\ nil, result \\ [])
+  defp determine_if_clinched([last], prev_points, result) do
+    points = get_current_points(last)
+    clinched = points < prev_points
+    Enum.reverse([clinched | result])
+  end
+  defp determine_if_clinched([record, next_record | data], prev_points, result) do
+    points = get_current_points(record)
+    below_opponent = get_potential_total_points(next_record)
+    max_points = get_potential_total_points(record)
+
+    clinched = points > below_opponent && (is_nil(prev_points) || max_points < prev_points)
+    determine_if_clinched([next_record | data], points, [clinched | result])
+  end
 end
