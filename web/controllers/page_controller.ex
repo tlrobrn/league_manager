3f4f9efaefe175e52730088f793e5d7b9fa91135
tlrobrn <obrien.taylorm@gmail.com>
2017-06-08 09:28:51 -0500

    fix trophy display

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,7 +4,7 @@ defmodule LeagueManager.PageController do
   def index(conn, _params) do
     games = LeagueManager.Game.current_games()
     records = LeagueManager.Team.records_with_teams_and_potential_points() |> Enum.to_list
-    [{{_, _}, %{points: leader_points}} | [{{_, nearest_potential_points}, _} | _]] = records
+    [{{_, _}, %{points: leader_points}} | [{{_, nearest_potential_points}, %{points: second_points}} | _]] = records
 
     render(
       conn,
@@ -12,7 +12,7 @@ defmodule LeagueManager.PageController do
       games: games,
       records: records,
       leader_points: leader_points,
-      clinched_league: leader_points > nearest_potential_points
+      clinched_league: leader_points > nearest_potential_points + second_points
     )
   end
 
