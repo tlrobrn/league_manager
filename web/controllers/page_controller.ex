3d8deba98bf1e890b52add69f6adea2cf8587b47
tlrobrn <obrien.taylorm@gmail.com>
2017-06-08 08:28:40 -0500

    add trophy once a team has clinched the league

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,10 +2,18 @@ defmodule LeagueManager.PageController do
   use LeagueManager.Web, :controller
 
   def index(conn, _params) do
-    records = LeagueManager.Team.records_with_teams_and_potential_points() |> Enum.to_list
-    [{{_team, _potential_points}, %{points: points}} | _] = records
     games = LeagueManager.Game.current_games()
-    render conn, "index.html", records: records, games: games, target_points: points
+    records = LeagueManager.Team.records_with_teams_and_potential_points() |> Enum.to_list
+    [{{_, _}, %{points: leader_points}} | [{{_, nearest_potential_points}, _} | _]] = records
+
+    render(
+      conn,
+      "index.html",
+      games: games,
+      records: records,
+      leader_points: leader_points,
+      clinched_league: leader_points > nearest_potential_points
+    )
   end
 
   def schedule(conn, _params) do
