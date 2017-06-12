9dc14e9afc8b198e3d79ccb4709f9199536a61e8
tlrobrn <obrien.taylorm@gmail.com>
2017-06-11 22:32:59 -0500

    fix for real?

--- web/views/view_helper.ex
+++ web/views/view_helper.ex
@@ -1,5 +1,5 @@
 defmodule LeagueManager.ViewHelper do
-  def image_url(team = %Team{logo: logo}) do
+  def image_url(team = %LeagueManager.Team{logo: logo}) do
     uri = LeagueManager.Logo.url({logo, team}) |> URI.parse
     [bucket, path] = uri.path |> String.trim_leading("/") |> String.split("/", parts: 2)
     %URI{uri | path: "/" <> path, host: bucket <> "." <> uri.host} |> URI.to_string
