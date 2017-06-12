8935a79937270d8079c83b895a2885cfa796f49d
tlrobrn <obrien.taylorm@gmail.com>
2017-06-11 22:12:31 -0500

    Revert "store in personal s3 fix 7"
    This reverts commit bdff0d6654f66144f5784440b97b7a2aa37370ef.

--- config/config.exs
+++ config/config.exs
@@ -30,7 +30,7 @@ config :arc,
 config :ex_aws,
   access_key_id: [{:system, "AWS_ACCESS_KEY_ID"}, :instance_role],
   secret_access_key: [{:system, "AWS_SECRET_ACCESS_KEY"}, :instance_role],
-  region: "us-east-1"
+  region: "us-east-2"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
