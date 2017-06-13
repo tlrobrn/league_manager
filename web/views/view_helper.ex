6fb744b45d80377349ef64333898c0f786f93647
tlrobrn <obrien.taylorm@gmail.com>
2017-06-13 12:07:37 -0500

    fix bug

--- web/views/view_helper.ex
+++ web/views/view_helper.ex
@@ -1,7 +1,11 @@
 defmodule LeagueManager.ViewHelper do
   def image_url(team = %LeagueManager.Team{logo: logo}) do
-    uri = LeagueManager.Logo.url({logo, team}) |> URI.parse
-    [bucket, path] = uri.path |> String.trim_leading("/") |> String.split("/", parts: 2)
-    %URI{uri | path: "/" <> path, host: bucket <> "." <> uri.host} |> URI.to_string
+    case LeagueManager.Logo.url({logo, team}) do
+      nil -> ""
+      url ->
+        uri = url |> URI.parse
+        [bucket, path] = uri.path |> String.trim_leading("/") |> String.split("/", parts: 2)
+        %URI{uri | path: "/" <> path, host: bucket <> "." <> uri.host} |> URI.to_string
+    end
   end
 end
